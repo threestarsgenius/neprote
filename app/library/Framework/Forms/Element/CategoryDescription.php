c82e40a232585a1e676ed98269fd57f7fcea4059
threestarsgenius <threestarsgenius@gmail.com>
2014-03-26 12:18:41 +0000

    I8: Use const of element name in all strings for custom forms element

--- app/library/Framework/Forms/Element/CategoryDescription.php
+++ app/library/Framework/Forms/Element/CategoryDescription.php
@@ -5,10 +5,17 @@ namespace Framework\Forms\Element;
 Class CategoryDescription extends \Phalcon\Forms\Element\TextArea {
 
 	const ELEMENT_NAME = 'description';
+	const ELEMENT_CLASS = 'form-control';
+	const ELEMENT_SIZE = 30;
+	const ELEMENT_MAXLENGTH = 255;
+	const ELEMENT_LABEL = 'Description';
 
 	public function __construct() {
-		parent::__construct(self::ELEMENT_NAME, array('class' => 'form-control', 'size' => '30', 'maxlength'=>255));
-		$this->setLabel('Description');
+		parent::__construct(self::ELEMENT_NAME, array(
+			'class' => self::ELEMENT_CLASS, 
+			'size' => self::ELEMENT_SIZE, 
+			'maxlength'=> self::ELEMENT_MAXLENGTH));
+		$this->setLabel(self::ELEMENT_LABEL);
 	}
 
 }
--- app/library/Framework/Forms/Element/Email.php
+++ app/library/Framework/Forms/Element/Email.php
@@ -5,15 +5,25 @@ namespace Framework\Forms\Element;
 Class Email extends \Phalcon\Forms\Element\Text {
 
 	const ELEMENT_NAME = 'email';
+	const ELEMENT_CLASS = 'form-control';
+	const ELEMENT_SIZE = 30;
+	const ELEMENT_MAXLENGTH = 70;
+	const ELEMENT_VALIDATOR_PRESENCEOF_TEXT = 'The email is required';
+	const ELEMENT_VALIDATOR_EMAIL_TEXT = 'The email is not valid';
+	const ELEMENT_LABEL = 'Email';
 
 	public function __construct() {
-		parent::__construct(self::ELEMENT_NAME, array('class' => 'form-control', 'size' => '30', 'maxlength'=>70));
+		parent::__construct(self::ELEMENT_NAME, array(
+			'class' => self::ELEMENT_CLASS, 
+			'size' => self::ELEMENT_SIZE, 
+			'maxlength'=> self::ELEMENT_MAXLENGTH));
 		$this->addValidators(array(
 				new \Phalcon\Validation\Validator\PresenceOf(array(
-						'message' => 'The email is required')),
+						'message' => self::ELEMENT_VALIDATOR_PRESENCEOF_TEXT)),
 				new \Phalcon\Validation\Validator\Email(array(
-						'message' => 'The email is not valid' ))
+						'message' => self::ELEMENT_VALIDATOR_EMAIL_TEXT))
 		));
+		$this->setLabel(self::ELEMENT_LABEL);
 	}
 
 }
